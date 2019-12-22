c8331ed865e9eb55f83904a553b3a30cd556359e
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-12-21 23:32:53 -0300

    Add files via upload

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,18 +1,24 @@
 const Usuario = require('../models/usuario');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
+const myPlaintextPassword = 's0P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+// const teste = "$2b$10$zI1j4WgwyO/VA5iYkAwawu8Io8qmwHwDsEoundkrwU/pCdyJ4yV9e";
 
 exports.create = function(req, res){
+    // The 'Matricula' and 'Senha' will passsed to hash function    
+    var hash = bcrypt.hashSync(req.body.Senha, saltRound);      //function that generate hash which will be stored on DB
     Usuario.create({
         Nome: req.body.Nome,
         Matricula: req.body.Matricula,
         Email: req.body.Email,
-        Senha: req.body.Senha
+        Senha: hash
     }).then(function(usuario){
         res.send(usuario);
     }).catch(function(err){
         res.send(err);
     });
 };
-
 
 exports.findOne = function(req, res){
     Usuario.findById(req.params.id).then(function(usuario){
@@ -52,3 +58,24 @@ exports.delete = function(req, res){
         res.send(err);
     });
 };
+
+exports.login = function(req, res){
+    nomeBody = req.body.Nome
+    matriculaBody = req.body.Matricula
+    emailBody = req.body.Email
+    senhaBody = req.body.Senha
+
+    Usuario.findOne({
+        where: {
+            Matricula: matriculaBody
+        }
+    }).then(function(usuario){
+        bcrypt.compare(senhaBody, usuario.Senha, function(err, resposta){
+            if(resposta){
+                res.send({authorizedLogin: true})
+            }else{
+                res.send({authorizedLogin: false})
+            }
+        })
+    })
+}
